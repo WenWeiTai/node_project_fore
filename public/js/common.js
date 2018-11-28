2b1626b9fe14edd16fffde7e2c6524383eed78cf
WenWeiTai <375354018@qq.com>
2018-11-28 14:39:45 +0800

    init

--- public/js/common.js
+++
@@ -1,9 +0,0 @@
-$('.addPhoneBtn').click(function(){
-    $('.masking').fadeIn(300);
-    $('.addItem_box').fadeIn(300);
-})
-
-$('.cancelBtn').click(function(){
-    $('.masking').fadeOut(300);
-    $('.addItem_box').fadeOut(300);
-})
--- public/js/head.js
+++
@@ -1,4 +0,0 @@
-$(".h-right > .btn").click(function(){
-    $(this).hide(200).prev().html('');
-    $(".btnHide").show(300);
-})
