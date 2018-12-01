dd8c594e861328c02cccd3eacd5b66a8a5aa35ae
WenWeiTai <375354018@qq.com>
2018-12-01 10:15:01 +0800

    代码改造

--- js/brand.js
+++ js/brand.js
@@ -1,173 +1,164 @@
-window.onload = function(){
-    obj = {
-        pagination :  $(".pagination"),
-        seachInput : $(".searchInput"),
-        seachBtn : $(".searchBtn"),
-        usersDateBox : $(".usersDateBox"),
-        navFirstLi : $(".pagination").children("li").first("li"),
-        addPhoneBtn : $('.addPhoneBtn'),
-    }
-    new OperationDate(obj);
-    
-
-}
-
-class OperationDate {
-    constructor(obj){
-        this.pagination = obj.pagination;
-        this.seachInput = obj.seachInput;
-        this.seachBtn = obj.seachBtn;
-        this.usersDateBox = obj.usersDateBox;
-        this.navFirstLi = obj.navFirstLi;
-        this.phoneDate = $('.phoneDate');
-        this.addPhoneBtn = obj.addPhoneBtn;
-        this.init()
-    }
-
-    init(){
-        this.getAjax();
-        this.delUser();
-        this.addPhone();
-    }
+$(function(){
+    operationDate.init();
+})
 
 
-    getAjax () {
-        var _this = this;
-        $.get("http://localhost:3000/api/brandDate", function (res) {
-            if (res.code == 1) {
-                _this.getUserDate(res);
-                _this.getNavLiData(res);
-            } else {
-                alert(res.msg);
-            }
-        });
-    }
-    
-    //用户数据渲染
-    getUserDate(res){
-        var userDatestr = "";
-        this.phoneDate.html('');
-        console.log(res)
-        for (var i = 0; i < res.data.length; i++) {
-            userDatestr += `
-            <tr _id=${res.data[i]._id}>
-                <th class="setTDLineH_brand" scope="row">${i + 1}</th>
-                <td><img src="${res.data[i].imgSrc}" alt="iphone" /></td>
-                <td class="setTDLineH_brand">${res.data[i].brand}</td>
-                <td class="setTDLineH_brand">
-                    <a class="td-update" href="##">修改</a>&ensp;&ensp;<a
-                    class="td-del"
-                    href="##"
-                    >删除</a
-                    >
-                </td>
-            </tr>
-            `;
-        }
-        this.phoneDate.html(userDatestr);
-    }
+var operationDate = (function(){
+    //分页ul
+    var pageUl =  $(".pagination");
+    //用户数据tbody
+    var phoneDateTbody = $(".phoneDate");
+    //分页第一个li
+    var navFirstLi = $(".pagination").children("li").first("li"); 
+    //添加手机按钮
+    var addPhoneBtn = $('.addPhoneBtn');
+    //确定按钮
+    var sureBtn = $('.sureBtn');
+    //当前页
+    var currentPage = 1;
+    //数据id
+    var _id = null;
+    //一页显示数据数量
+    var pageSize = 3;
 
-    //删除
-    delUser(){
-        var _this = this;
-        this.phoneDate.on('click','.td-del',function(){
-            var id = $(this).parent().parent().attr('_id');
-            console.log(id)
-            $.get('http://localhost:3000/api/brandDelete',{id : id},function(res){
-                console.log(res)
-                if(res.code == 1){
-                    _this.getAjax ()
-                }else{
+    var navLiS = null;
+
+    return {
+        //初始化
+        init(){
+            this.event();
+            this.getDate(currentPage,pageSize)
+        },
+        //获取数据
+        getDate(currentPage,pageSize){
+            var _this = this;
+            //请求数据
+            $.get("http://localhost:3000/api/brandDate?page="+ currentPage + "&pageSize=" + pageSize, function (res) {
+                if (res.code == 1) {
+                    //获取数据后渲染用户数据和分页
+                    _this.setDate(res)
+                } else {
                     alert(res.msg);
                 }
+            });
+        },
+
+        //渲染数据
+        setDate(res){
+            //用户数据模版
+            var userDatestr = "";
+            //分页数据模版
+            var navLiStr = "";
+            
+            //====用户数据渲染
+            //渲染用户数据
+            for (var i = 0; i < res.data.length; i++) {
+                userDatestr += `
+                <tr _id=${res.data[i]._id}>
+                    <th class="setTDLineH_brand" scope="row">${i + 1}</th>
+                    <td><img src="${res.data[i].imgSrc}" alt="iphone" /></td>
+                    <td class="setTDLineH_brand">${res.data[i].brand}</td>
+                    <td class="setTDLineH_brand">
+                        <a class="td-update" href="##">修改</a>&ensp;&ensp;<a
+                        class="td-del"
+                        href="##"
+                        >删除</a
+                        >
+                    </td>
+                </tr>
+                `;
+            }
+            phoneDateTbody.html(userDatestr);
+            //管理员没有删除按钮
+            $('.del_hide').remove();
+
+            //====分页数据渲染
+            //获取分页
+            var liS =  $('.firstLi').siblings('li').not('.lastLi');
+            //每次重新渲染前清空
+            if(liS.length > 0){
+                liS.remove();
+            }
+            //渲染分页
+            for (var i = 0; i < res.totalPage; i++) {
+                navLiStr += `
+                <li class="${currentPage == i + 1 ? "active" : ''}"><a href="#">${i + 1}</a></li>
+                `
+            }
+            //在第一个li后面插入
+            navFirstLi.after(navLiStr);
+            //获取渲染后分页(包括前后)的li数量
+            navLiS = $(".pagination li")
+        },
+
+        //事件
+        event(){
+            var _this = this;
+
+            //分页点击功能
+            pageUl.on('click','li',function(){
+                //获取点击的下标
+                var index = $(this).index()
+                //前
+                if(index == 0){
+                    currentPage = currentPage - 1 < 1 ? 1 : currentPage - 1
+                //后
+                }else if(index == navLiS.size() - 1){
+                    currentPage = currentPage + 1 > navLiS.size() - 2 ? navLiS.size() - 2 : currentPage + 1
+                //数字页
+                }else{
+                    currentPage = index
+                }
+                //渲染当前页数据
+                _this.getDate(currentPage,pageSize)
             })
 
-        })
-    }
-
-
-   //分页渲染
-   getNavLiData(res){
-        var navLiStr = "";
-        this.liS =  $('.firstLi').siblings('li').not('.lastLi')
-        if(this.liS.length > 0){
-            this.liS.remove();
-        }
-        for (var i = 0; i < res.totalPage; i++) {
-            navLiStr += `
-            <li class="${0 == i ? "active" : ''}"><a href="#">${i + 1}</a></li>
-            `
-        }
-        this.navFirstLi.after(navLiStr); 
-        this.clickNavLi();
-    }
-
-    //分页操作
-    clickNavLi(){
-        //获取渲染后分页(包括前后)的li数量
-        var navLiS = $(".pagination li")
-        //保存每次点击的下标
-        var clickIndex = 1;
-        var _this = this;
-        this.pagination.on('click','li',function(){
-            //获取点击的下标
-            var index = $(this).index()
-            //前
-            if(index < 1){
-                clickIndex = clickIndex - 1 < 1 ? 1 : clickIndex - 1;
-                _this.clickNavAjax(navLiS,clickIndex);
-            //后
-            }else if(index == navLiS.size() - 1){
-                clickIndex = clickIndex + 1 > navLiS.size() - 2 ? navLiS.size() - 2 : clickIndex + 1;
-                _this.clickNavAjax(navLiS,clickIndex);
-            //数字页
-            }else{
-                clickIndex = index;
-                _this.clickNavAjax(navLiS,clickIndex);
-            }
-        })
-    }
-
-    //分页数据请求
-    clickNavAjax(navLiS,clickIndex){
-        var _this = this;
-        $.get("http://localhost:3000/api/brandDate?page="+ clickIndex +"&pageSize=3",function(res){
-            _this.getUserDate(res)
-        });
-        navLiS.eq(clickIndex).addClass('active').siblings().removeClass('active')
-    }
-
-    //添加品牌
-    addPhone(){
-        var _this = this;
-        $('.sureBtn').click(function(e){
-            e.preventDefault();
-            //拿到所有值
-            var pBrandVal = $('.pBrand').val();
-            var pFile = document.getElementById('pFile').files[0];
-
-
-            if(pBrandVal && pFile){
-                var obj = new FormData();
-                obj.append('imgFile',pFile);
-                obj.append('pBrand',pBrandVal);
-
-                $.ajax({
-                    type : 'post',
-                    url : 'http://localhost:3000/api/addbrand',
-                    data : obj,
-                    contentType : false,
-                    processData : false,
-                    success : function(res){
-                        if(res.code == 1){
-                            $('.cancelBtn').click();
-                            alert('添加手机成功');
-                            _this.getAjax()
-                        }
+            //删除
+            phoneDateTbody.on('click','.td-del',function(){
+                //获取id
+                _id = $(this).parent().parent().attr('_id');
+                //获取当前页
+                currentPage = pageUl.find('.active').index();
+                $.get('http://localhost:3000/api/brandDelete',{_id : _id},function(res){
+                    if(res.code == 1){
+                        alert(res.msg);
+                        //重新渲染数据
+                        _this.getDate(currentPage,pageSize)
+                    }else{
+                        alert(res.msg);
                     }
                 })
-            }
-            console.log(pBrandVal,pFile)
-        })
+            })
+
+            //添加品牌
+            sureBtn.click(function(e){
+                e.preventDefault();
+                //拿到所有值
+                var pBrandVal = $('.pBrand').val();
+                var pFile = document.getElementById('pFile').files[0];
+
+                if(pBrandVal && pFile){
+                    var obj = new FormData();
+                    obj.append('imgFile',pFile);
+                    obj.append('pBrand',pBrandVal);
+    
+                    $.ajax({
+                        type : 'post',
+                        url : 'http://localhost:3000/api/addbrand',
+                        data : obj,
+                        contentType : false,
+                        processData : false,
+                        success : function(res){
+                            if(res.code == 1){
+                                $('.cancelBtn').click();
+                                alert('添加手机成功');
+                                _this.getDate(currentPage,pageSize)
+                            }
+                        }
+                    })
+                }
+                console.log(pBrandVal,pFile)
+            })
+        }
     }
-}
+})()
--- js/phone.js
+++ js/phone.js
@@ -1,57 +1,58 @@
-window.onload = function(){
-    obj = {
-        pagination :  $(".pagination"),
-        seachInput : $(".searchInput"),
-        seachBtn : $(".searchBtn"),
-        usersDateBox : $(".usersDateBox"),
-        navFirstLi : $(".pagination").children("li").first("li"),
-        addPhoneBtn : $('.addPhoneBtn'),
-    }
-    new OperationDate(obj);
-    
-
-}
-
-class OperationDate {
-    constructor(obj){
-        this.pagination = obj.pagination;
-        this.seachInput = obj.seachInput;
-        this.seachBtn = obj.seachBtn;
-        this.usersDateBox = obj.usersDateBox;
-        this.navFirstLi = obj.navFirstLi;
-        this.phoneDate = $('.phoneDate');
-        this.addPhoneBtn = obj.addPhoneBtn;
-        this.init()
-    }
-
-    init(){
-        this.getAjax();
-        this.delUser();
-        this.addPhone();
-    }
+$(function(){
+    operationDate.init();
+})
 
 
-    getAjax () {
-        var _this = this;
-        $.get("http://localhost:3000/api/phoneDate", function (res) {
-            if (res.code == 1) {
-                _this.getUserDate(res);
-                _this.getNavLiData(res);
-            } else {
-                alert(res.msg);
-            }
-        });
-    }
-    
-    //用户数据渲染
-    getUserDate(res){
-        var userDatestr = "";
-        this.phoneDate.html('');
-        var _this = this;
-        console.log(res)
-        for (var i = 0; i < res.data.length; i++) {
-            userDatestr += `
-            <tr _id="${res.data[i]._id}">    
+var operationDate = (function(){
+    //分页ul
+    var pageUl =  $(".pagination");
+    //用户数据tbody
+    var phoneDateTbody = $(".phoneDate");
+    //分页第一个li
+    var navFirstLi = $(".pagination").children("li").first("li"); 
+    //添加手机确定按钮
+    var sureBtn = $('.sureBtn');
+    //当前页
+    var currentPage = 1;
+    //数据id
+    var _id = null;
+    //一页显示数据数量
+    var pageSize = 3;
+
+    var navLiS = null;
+
+    return {
+        //初始化
+        init(){
+            this.event();
+            this.getDate(currentPage,pageSize)
+        },
+        //获取数据
+        getDate(currentPage,pageSize){
+            var _this = this;
+            //请求数据
+            $.get("http://localhost:3000/api/phoneDate?page="+ currentPage + "&pageSize=" + pageSize, function (res) {
+                if (res.code == 1) {
+                    //获取数据后渲染用户数据和分页
+                    _this.setDate(res)
+                } else {
+                    alert(res.msg);
+                }
+            });
+        },
+
+        //渲染数据
+        setDate(res){
+            //用户数据模版
+            var userDatestr = "";
+            //分页数据模版
+            var navLiStr = "";
+            
+            //====用户数据渲染
+            //渲染用户数据
+            for (var i = 0; i < res.data.length; i++) {
+                userDatestr += `
+                <tr _id="${res.data[i]._id}">    
                 <th class="setTDLineH" scope="row">${i + 1}</th>
                 <td>
                     <img
@@ -72,143 +73,125 @@ class OperationDate {
                     >
                 </td>
             </tr>
-            `;
-        }
-        this.phoneDate.html(userDatestr);
-
-        //修改信息
-        this.phoneDate.on('click','.td-update',function(){
-            //弹出修改窗口
-            $('.masking').fadeIn(300);
-            $('.addItem_box').fadeIn(300).find('h2').html('修改信息');
-            //获取当前点击行的值
-            $('.pName').val($(this).parent().parent().children().eq(2).html());
-            $('.pBrand').val($(this).parent().parent().children().eq(3).html());
-            $('.pPrice').val($(this).parent().parent().children().eq(4).html());
-            $('.pSecondPrice').val($(this).parent().parent().children().eq(5).html());
-            
-            //调用添加数据的方法更新当前数据
-            // _this.addPhone('http://localhost:3000/api/updataPhone','修改信息成功')
-            //点击取消隐藏修改窗口
-            $('.cancelBtn').click(function(){
-                $('.masking').fadeOut(300);
-                $('.addItem_box').fadeOut(300);
-            })
-        })
-        
-    }
-
-    //删除
-    delUser(){
-        var _this = this;
-        this.phoneDate.on('click','.td-del',function(){
-            var id = $(this).parent().parent().attr('_id');
-            
-            $.get('http://localhost:3000/api/phoneDelete',{id : id},function(res){
-                console.log(res)
-                if(res.code == 1){
-                    _this.getAjax ()
-                }else{
-                    alert(res.msg);
-                }
-            })
-
-        })
-    }
-
-
-   //分页渲染
-   getNavLiData(res){
-        var navLiStr = "";
-        this.liS =  $('.firstLi').siblings('li').not('.lastLi')
-        if(this.liS.length > 0){
-            this.liS.remove();
-        }
-        for (var i = 0; i < res.totalPage; i++) {
-            navLiStr += `
-            <li class="${0 == i ? "active" : ''}"><a href="#">${i + 1}</a></li>
-            `
-        }
-        this.navFirstLi.after(navLiStr); 
-        this.clickNavLi();
-    }
-
-    //分页操作
-    clickNavLi(){
-        //获取渲染后分页(包括前后)的li数量
-        var navLiS = $(".pagination li")
-        //保存每次点击的下标
-        var clickIndex = 1;
-        var _this = this;
-        this.pagination.on('click','li',function(){
-            //获取点击的下标
-            var index = $(this).index()
-            //前
-            if(index < 1){
-                clickIndex = clickIndex - 1 < 1 ? 1 : clickIndex - 1;
-                _this.clickNavAjax(navLiS,clickIndex);
-            //后
-            }else if(index == navLiS.size() - 1){
-                clickIndex = clickIndex + 1 > navLiS.size() - 2 ? navLiS.size() - 2 : clickIndex + 1;
-                _this.clickNavAjax(navLiS,clickIndex);
-            //数字页
-            }else{
-                clickIndex = index;
-                _this.clickNavAjax(navLiS,clickIndex);
+                `;
             }
-        })
-    }
+            phoneDateTbody.html(userDatestr);
+            
+            //====分页数据渲染
+            //获取分页
+            var liS =  $('.firstLi').siblings('li').not('.lastLi');
+            //每次重新渲染前清空
+            if(liS.length > 0){
+                liS.remove();
+            }
+            //渲染分页
+            for (var i = 0; i < res.totalPage; i++) {
+                navLiStr += `
+                <li class="${currentPage == i + 1 ? "active" : ''}"><a href="#">${i + 1}</a></li>
+                `
+            }
+            //在第一个li后面插入
+            navFirstLi.after(navLiStr);
+            //获取渲染后分页(包括前后)的li数量
+            navLiS = $(".pagination li")
+        },
 
-    //分页数据请求
-    clickNavAjax(navLiS,clickIndex){
-        var _this = this;
-        $.get("http://localhost:3000/api/phoneDate?page="+ clickIndex +"&pageSize=3",function(res){
-            _this.getUserDate(res)
-        });
-        navLiS.eq(clickIndex).addClass('active').siblings().removeClass('active')
-    }
+        //事件
+        event(){
+            var _this = this;
 
-    //添加手机
-    addPhone(url,msg){
-        url = url || 'http://localhost:3000/api/addPhone',
-        msg = msg || '添加手机成功'
-        var _this = this;
-        $('.sureBtn').click(function(e){
-            e.preventDefault();
-            //拿到所有值
-            var pNameVal = $('.pName').val();
-            var pBrandVal = $('.pBrand').val();
-            var pPriceVal = $('.pPrice').val();
-            var pSecondPriceVal = $('.pSecondPrice').val();
-            var pFile = document.getElementById('pFile').files[0];
+            //分页点击功能
+            pageUl.on('click','li',function(){
+                //获取点击的下标
+                var index = $(this).index()
+                //前
+                if(index == 0){
+                    currentPage = currentPage - 1 < 1 ? 1 : currentPage - 1
+                //后
+                }else if(index == navLiS.size() - 1){
+                    currentPage = currentPage + 1 > navLiS.size() - 2 ? navLiS.size() - 2 : currentPage + 1
+                //数字页
+                }else{
+                    currentPage = index
+                }
+                //渲染当前页数据
+                _this.getDate(currentPage,pageSize)
+            })
 
-
-            if(pNameVal && pBrandVal && pPriceVal && pSecondPriceVal && pFile){
-                var obj = new FormData();
-                obj.append('imgFile',pFile);
-                obj.append('pName',pNameVal);
-                obj.append('pBrand',pBrandVal);
-                obj.append('pPrice',pPriceVal);
-                obj.append('pSecondPrice',pSecondPriceVal);
-
-                $.ajax({
-                    type : 'post',
-                    url : url,
-                    data : obj,
-                    contentType : false,
-                    processData : false,
-                    success : function(res){
-                        if(res.code == 1){
-                            $('.cancelBtn').click();
-                            alert(msg);
-                            _this.getAjax()
-                        }
+            //删除
+            phoneDateTbody.on('click','.td-del',function(){
+                //获取id
+                _id = $(this).parent().parent().attr('_id');
+                //获取当前页
+                currentPage = pageUl.find('.active').index();
+                $.get('http://localhost:3000/api/phoneDelete',{_id : _id},function(res){
+                    if(res.code == 1){
+                        alert(res.msg);
+                        //重新渲染数据
+                        _this.getDate(currentPage,pageSize)
+                    }else{
+                        alert(res.msg);
                     }
                 })
-            }else{
-                alert('请输入完整');
-            }
-            console.log(pNameVal,pBrandVal,pPriceVal,pSecondPriceVal,pFile)
-        })
+            })
+
+            //添加品牌
+            sureBtn.click(function(e){
+                e.preventDefault();
+                //拿到所有值
+                var pNameVal = $('.pName').val();
+                var pBrandVal = $('.pBrand').val();
+                var pPriceVal = $('.pPrice').val();
+                var pSecondPriceVal = $('.pSecondPrice').val();
+                var pFile = document.getElementById('pFile').files[0];
+
+                if(pNameVal && pBrandVal && pPriceVal && pSecondPriceVal && pFile){
+                    var obj = new FormData();
+                    obj.append('imgFile',pFile);
+                    obj.append('pName',pNameVal);
+                    obj.append('pBrand',pBrandVal);
+                    obj.append('pPrice',pPriceVal);
+                    obj.append('pSecondPrice',pSecondPriceVal);
+    
+                    $.ajax({
+                        type : 'post',
+                        url : 'http://localhost:3000/api/addPhone',
+                        data : obj,
+                        contentType : false,
+                        processData : false,
+                        success : function(res){
+                            if(res.code == 1){
+                                $('.cancelBtn').click();
+                                alert('添加手机成功');
+                                _this.getDate(currentPage,pageSize)
+                            }
+                        }
+                    })
+                }else{
+                    alert('请输入完整');
+                }
+                console.log(pBrandVal,pFile)
+            })
+
+            //修改信息
+            /* this.phoneDate.on('click','.td-update',function(){
+                //弹出修改窗口
+                $('.masking').fadeIn(300);
+                $('.addItem_box').fadeIn(300).find('h2').html('修改信息');
+                //获取当前点击行的值
+                $('.pName').val($(this).parent().parent().children().eq(2).html());
+                $('.pBrand').val($(this).parent().parent().children().eq(3).html());
+                $('.pPrice').val($(this).parent().parent().children().eq(4).html());
+                $('.pSecondPrice').val($(this).parent().parent().children().eq(5).html());
+                
+                //调用添加数据的方法更新当前数据
+                // _this.addPhone('http://localhost:3000/api/updataPhone','修改信息成功')
+                //点击取消隐藏修改窗口
+                $('.cancelBtn').click(function(){
+                    $('.masking').fadeOut(300);
+                    $('.addItem_box').fadeOut(300);
+                })
+            }) */
+        }
     }
-}
+})()
